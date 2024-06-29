3c32bb43dd01e4fec81102438007ded3a695aabc
Sudakova Mariia <littlezwhitezridingzhood@gmail.com>
2024-06-29 15:59:49 +0300

    Add files via upload

--- appreviewmodel.py
+++ appreviewmodel.py
@@ -1,5 +1,7 @@
 # for data analysis
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # for models
 from sklearn.utils import resample
@@ -9,6 +11,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import classification_report
+from wordcloud import WordCloud
 
 
 class AppReviewModel:
@@ -28,15 +31,16 @@ class AppReviewModel:
         return balanced_upsampled
 
     def __init__(self, path_to_data_for_model: str):
-        df = pd.read_csv(path_to_data_for_model)
+        self.df = pd.read_csv(path_to_data_for_model)
 
-        X_train, X_test, y_train, y_test = train_test_split(df['processed_review'], df['Sentiment'], test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(self.df['processed_review'], self.df['Sentiment'], test_size=0.2, random_state=42)
 
         train_data = pd.concat([X_train, y_train], axis=1)
 
         # Балансировка данных
-        balanced_upsampled = self.prepare_data(train_data)
+        self.balanced_upsampled = self.prepare_data(train_data)
 
+    def turn_sentiment_prediction(self):
         pipeline = Pipeline([
             ('tfidf', TfidfVectorizer()),
             ('scaler', StandardScaler(with_mean=False)),
@@ -55,7 +59,7 @@ class AppReviewModel:
         self.model = GridSearchCV(pipeline, param_grid, cv=5, n_jobs=-1, verbose=1)
 
         # Обучение модели на сбалансированных данных
-        self.model.fit(balanced_upsampled['processed_review'], balanced_upsampled['Sentiment'])
+        self.model.fit(self.balanced_upsampled['processed_review'], self.balanced_upsampled['Sentiment'])
 
     def predict_sentiment(self, X_data: pd.Series, true_y_data: pd.Series, show_report: bool):
         predictions = self.model.predict(X_data)
@@ -67,3 +71,25 @@ class AppReviewModel:
             print("Best parameters found: ", self.model.best_params_)
         
         return predictions
+    
+    def create_wordcloud(self, isPositive: bool):
+        if isPositive:
+            # Wordcloud для положительных отзывов
+            positive_reviews = ' '.join(self.df[self.df['Sentiment'] == 'Positive']['processed_review'])
+            positive_wordcloud = WordCloud(background_color='white', colormap='Greens').generate(positive_reviews)
+
+            plt.figure(figsize=(10, 6))
+            plt.imshow(positive_wordcloud, interpolation='bilinear')
+            plt.axis('off')
+            plt.title('Wordcloud for Positive Reviews')
+            plt.show()
+        else:
+            # Wordcloud для негативных отзывов
+            negative_reviews = ' '.join(df[df['Sentiment'] == 'Negative']['processed_review'])
+            negative_wordcloud = WordCloud(background_color='white', colormap='Reds').generate(positive_reviews)
+
+            plt.figure(figsize=(10, 6))
+            plt.imshow(negative_wordcloud, interpolation='bilinear')
+            plt.axis('off')
+            plt.title('Wordcloud for Negative Reviews')
+            plt.show()
